38b5daf1cb5ccf0c815d4cd26fd046643944dc2a
Toby <tobylofas@gmail.com>
2020-05-08 20:15:57 +0100

    Cleaned up main file. Deleted obsolete package.json.

--- src/main.js
+++ src/main.js
@@ -3,34 +3,28 @@ import InputHandler from "./input";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
-
 const SCREEN_WIDTH = 800;
 const SCREEN_HEIGHT = 600;
+
+let deltaTime;
+let fps;
+let timestep = 1000 / 60;
+let lastTime = 0;
 
 let player = new Player(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 new InputHandler(player);
 
-let deltaTime;
-
-let fps;
-let timestep = 1000 / 60;
-
-let lastTime = 0;
-
 function gameLoop(timestamp) {
-  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);  //clears screen
   deltaTime = timestamp - lastTime;
   lastTime = timestamp;
-
-  player.update(deltaTime);
-
-  player.draw(ctx);
-
   fps = 1000 / deltaTime;
 
-  //Calls next frame. Needed for deltaTime. Needs to happen at end of game loop
-  requestAnimationFrame(gameLoop);
+  player.update(deltaTime);
+  player.draw(ctx);
+
+  requestAnimationFrame(gameLoop); //Calls next frame. Needed for deltaTime. Needs to happen at end of game loop
 }
 
 gameLoop();
